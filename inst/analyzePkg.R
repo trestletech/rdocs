c34ab2c59c18d75bfd53a7e548be170832c48dc4
trestletech <jeff.allen@trestletechnology.net>
2019-08-29 13:56:36 -0500

    Consider NAMESPACE export of functions
    False(?) positive rate went from 21, 17, 4% to 15, 3, 0% for Shiny, ggplot2, and plumber, respectively.
    And the 15 on Shiny appears largely to be attributable to all of the
    tags that are indeed exported that rcheologist missed.

--- inst/analyzePkg.R
+++ inst/analyzePkg.R
@@ -5,7 +5,7 @@ classSep <- "__CLASS__"
 analyzePkg <- function(pkg, rlib, ver){
   cat("\t", pkg, "\n")
   rds <- list.files(file.path(rlib, pkg, "man"))
-  lapply(rds, function(rd){
+  allobjs <- lapply(rds, function(rd){
     #cat("\t\t", rd, "\n")
     rd_path <- file.path(rlib, pkg, "man", rd)
     p <- tools::parse_Rd(rd_path)
@@ -18,113 +18,141 @@ analyzePkg <- function(pkg, rlib, ver){
     # TODO: some classes aren't getting pulled out properly. e.g. utils::printBibtex should be
     #       `print` (class Bibtex)
 
-    usage <- p[sapply(p, function(x){attr(x, "Rd_tag") == "\\usage"})]
-    if (length(usage) == 0){
-      return()
-    }
-
-    # Replace \\dots with ...
-    idx <- vapply(usage[[1]], function(x){ attr(x, "Rd_tag") == "\\dots"}, logical(1))
-    usage[[1]][idx] <- "..."
-
-    # The class of S3 methods appears as the second element in the list. i.e. in relist.Rd 3.5.2:
-    # [[3]]
-    # [[3]][[1]]
-    # [[3]][[1]][[1]]
-    # [1] "relist"
-    # attr(,"Rd_tag")
-    # [1] "TEXT"
-    #
-    #
-    # [[3]][[2]]
-    # [[3]][[2]][[1]]
-    # [1] "default"
-    # attr(,"Rd_tag")
-    # [1] "TEXT"
-    # We want to filter out these second elements when we see this.
-    usage[[1]] <- lapply(usage[[1]], function(u){
-      if (identical(attr(u, "Rd_tag"), "\\method")) {
-        # Combine the two elements into one using a magic string to help us later
-        # identify that this is a special function.
-        return(paste(as.character(unlist(u)), collapse=classSep))
-      }
-      u
-    })
-
-    str <- unlist(usage[[1]])
-
-    #idx <- vapply(usage[[1]], function(x){ length(x) >= 1 && x != "\n"}, logical(1))
-    #str <- unlist(usage[[1]][idx])
-
-    # filter out comments (e.g. in base::base-internal.Rd)
-    str <- str[!grepl("^%.*", str, perl = TRUE)]
-
-    fil <- tempfile(fileext = ".Rdmped")
-    cat(paste(str, collapse=""), file = fil)
-    # TODO: you can recover from partially parsed files. See ?parse
-    par <- tryCatch({parse(fil=fil)}, error=function(x){
-      warning(pkg, "::", rd, ": ", x)
-      NULL
-    })
-    unlink(fil)
-
     defunct <- FALSE
     # TODO: is this regex robust?
     if (grepl("-defunct\\W", rd, perl=TRUE)){
       defunct <- TRUE
     }
 
-    usages <- lapply(par, function(pa){
-      # NULL is documented (see ?NULL) which really throws everything off. Treat this as a special case
-      if (is.null(pa)){
-        return(data.frame(ver=ver, pkg=pkg, file=rd, fun="NULL", class=NA_character_, paramName=NA_character_, placeholder=NA_character_, evaluated=FALSE, defunct=defunct, stringsAsFactors = FALSE))
-      }
+    usage <- p[sapply(p, function(x){attr(x, "Rd_tag") == "\\usage"})]
+    usages <- NULL
+    if (length(usage) > 0){
+      # Replace \\dots with ...
+      idx <- vapply(usage[[1]], function(x){ attr(x, "Rd_tag") == "\\dots"}, logical(1))
+      usage[[1]][idx] <- "..."
 
-      if (is.symbol(pa)){
-        # Example may just be an unevaluated call.
-        # we'll just represent this with evaluated=FALSE
-        return(data.frame(ver=ver, pkg=pkg, file=rd, fun=as.character(pa), class=NA_character_, paramName=NA_character_, placeholder=NA_character_, evaluated=FALSE, defunct=defunct, stringsAsFactors = FALSE))
-      }
-
-      fname <- as.character(pa[[1]])
-      class <- NA_character_
-      if (grepl(classSep, fname)){
-        re <- paste0("^(.*)", classSep, "(.*)$")
-        mat <- stringr::str_match(fname, re)
-
-        if (nrow(mat) > 0){
-          fname <- mat[1,2]
-          class <- mat[1,3]
+      # The class of S3 methods appears as the second element in the list. i.e. in relist.Rd 3.5.2:
+      # [[3]]
+      # [[3]][[1]]
+      # [[3]][[1]][[1]]
+      # [1] "relist"
+      # attr(,"Rd_tag")
+      # [1] "TEXT"
+      #
+      #
+      # [[3]][[2]]
+      # [[3]][[2]][[1]]
+      # [1] "default"
+      # attr(,"Rd_tag")
+      # [1] "TEXT"
+      # We want to filter out these second elements when we see this.
+      usage[[1]] <- lapply(usage[[1]], function(u){
+        if (identical(attr(u, "Rd_tag"), "\\method")) {
+          # Combine the two elements into one using a magic string to help us later
+          # identify that this is a special function.
+          return(paste(as.character(unlist(u)), collapse=classSep))
         }
-      }
+        u
+      })
 
-      # Only explore if there are actually arguments.
-      placeholders <- NA_character_
-      pnames <- NA_character_
-      if (length(pa) > 1){
-        placeholders <- as.character(pa[2:length(pa)])
-        pnames <- names(pa)[2:length(pa)]
+      str <- unlist(usage[[1]])
 
-        # If no names, then the stated characters are the names of the parameters.
-        if (is.null(pnames)){
-          pnames <- placeholders
-          placeholders <- rep(NA_character_, times = length(pa)-1)
+      #idx <- vapply(usage[[1]], function(x){ length(x) >= 1 && x != "\n"}, logical(1))
+      #str <- unlist(usage[[1]][idx])
+
+      # filter out comments (e.g. in base::base-internal.Rd)
+      str <- str[!grepl("^%.*", str, perl = TRUE)]
+
+      fil <- tempfile(fileext = ".Rdmped")
+      cat(paste(str, collapse=""), file = fil)
+      # TODO: you can recover from partially parsed files. See ?parse
+      par <- tryCatch({parse(fil=fil)}, error=function(x){
+        warning(pkg, "::", rd, ": ", x)
+        NULL
+      })
+      unlink(fil)
+
+      usages <- lapply(par, function(pa){
+        # NULL is documented (see ?NULL) which really throws everything off. Treat this as a special case
+        if (is.null(pa)){
+          return(data.frame(ver=ver, pkg=pkg, file=rd, fun="NULL", class=NA_character_, paramName=NA_character_, placeholder=NA_character_, evaluated=FALSE, defunct=defunct, stringsAsFactors = FALSE))
         }
 
-        # Where unnamed, we have no placeholder and the given text is the pname
-        noplace <- pnames == ""
-        pnames[noplace] <- placeholders[noplace]
-        placeholders[noplace] <- NA_character_
-      }
+        if (is.symbol(pa)){
+          # Example may just be an unevaluated call.
+          # we'll just represent this with evaluated=FALSE
+          return(data.frame(ver=ver, pkg=pkg, file=rd, fun=as.character(pa), class=NA_character_, paramName=NA_character_, placeholder=NA_character_, evaluated=FALSE, defunct=defunct, stringsAsFactors = FALSE))
+        }
 
-      # TODO: see R 3.5.2's utils::relist.Rd for an example of a documented, unexported function: unlist.relistable
+        fname <- as.character(pa[[1]])
+        class <- NA_character_
+        if (grepl(classSep, fname)){
+          re <- paste0("^(.*)", classSep, "(.*)$")
+          mat <- stringr::str_match(fname, re)
 
-      data.frame(ver=ver, pkg=pkg, file=rd, fun=fname, class=class, paramName=pnames, placeholder=placeholders, evaluated=TRUE, defunct=defunct, stringsAsFactors = FALSE)
-    }) %>% bind_rows()
+          if (nrow(mat) > 0){
+            fname <- mat[1,2]
+            class <- mat[1,3]
+          }
+        }
+
+        # Only explore if there are actually arguments.
+        placeholders <- NA_character_
+        pnames <- NA_character_
+        if (length(pa) > 1){
+          placeholders <- as.character(pa[2:length(pa)])
+          pnames <- names(pa)[2:length(pa)]
+
+          # If no names, then the stated characters are the names of the parameters.
+          if (is.null(pnames)){
+            pnames <- placeholders
+            placeholders <- rep(NA_character_, times = length(pa)-1)
+          }
+
+          # Where unnamed, we have no placeholder and the given text is the pname
+          noplace <- pnames == ""
+          pnames[noplace] <- placeholders[noplace]
+          placeholders[noplace] <- NA_character_
+        }
+
+        # TODO: see R 3.5.2's utils::relist.Rd for an example of a documented, unexported function: unlist.relistable
+
+        data.frame(ver=ver, pkg=pkg, file=rd, fun=fname, class=class, paramName=pnames, placeholder=placeholders, evaluated=TRUE, defunct=defunct, stringsAsFactors = FALSE)
+      }) %>% bind_rows()
+    }
 
     # combine usages and aliasNames
-    aliasDF <- data.frame(ver=ver, pkg=pkg, file=rd, fun=aliasNames, class=NA_character_, paramName=NA_character_, placeholder=NA_character_, evaluated=NA, defunct=defunct, stringsAsFactors = FALSE)
-    aliasDF <- aliasDF %>% filter(! fun %in% usages$fun)
-    bind_rows(usages, aliasDF)
+    if (length(aliasNames) > 0){
+      aliasDF <- data.frame(ver=ver, pkg=pkg, file=rd, fun=aliasNames, class=NA_character_, paramName=NA_character_, placeholder=NA_character_, evaluated=NA, defunct=defunct, stringsAsFactors = FALSE)
+      aliasDF <- aliasDF %>% filter(! fun %in% usages$fun)
+      bind_rows(usages, aliasDF)
+    } else {
+      usages
+    }
   }) %>% bind_rows()
+
+  # Supplement with information about whether or not this object is exported
+  ns <- base::parseNamespaceFile(pkg, rlib, mustExist = FALSE)
+
+  addExported(allobjs, ns)
+}
+
+# Supplements a data.frame with information about whether or not each object is exported
+addExported <- function(objs, ns){
+  # First, the simple exports
+  objs <- objs %>% mutate(exported = is.na(class) & fun %in% ns$exports)
+
+  # Now the patterns
+  matches <- vapply(objs$fun, function(f){
+    matches <- vapply(ns$exportPatterns, function(ep){
+      grepl(ep, f, perl=TRUE)
+    }, logical(1))
+    any(matches)
+  }, logical(1))
+  objs <- objs %>% mutate(exported = exported | (matches & is.na(class)))
+
+  # TODO: we could consider adding any explicitly named export that we haven't yet observed.
+  # TODO: handle S3 classes -- or maybe those don't count as exported?
+
+  objs
 }
